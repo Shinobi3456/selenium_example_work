b45f159d0271b2620bab9797bc6e39605b4055be
v_razvodov <shinobi3456@gmail.com>
2020-08-14 19:42:37 +0500

    add  script booking accommodation

---
+++ lesson26_step2.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import time
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = "http://suninjuly.github.io/explicit_wait2.html"
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+browser = webdriver.Chrome()
+try:
+    browser.get(link)
+
+    WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), '$100')
+    )
+    
+    book = browser.find_element_by_id('book')
+    book.click()
+    
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    input = browser.find_element_by_id('answer')
+    input.send_keys(y)
+
+    buttonSubmit = browser.find_element_by_css_selector('button[type="submit"]')
+    buttonSubmit.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
